8e7114e12bc2c951d98fb666fc1af37d3d0ef8dd
Виктория Лейбуш <viktoriia.leibush@monopoly.su>
2021-09-10 21:56:35 +0300

    added SelenideGithubTest and DragAndDropTest

---
+++ src/test/java/com/demoqa/DragAndDropTest.java
@@ -0,0 +1,24 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+    }
+}
---
+++ src/test/java/com/demoqa/SelenideGithubTest.java
@@ -0,0 +1,29 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideGithubTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void searchSoftAssertions() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class"), text("@ExtendWith"));
+    }
+
+
+}
